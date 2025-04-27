7fa662a8b7ba887b1f9b5b23e2acb4b0d5a1858f
Tristan7122 <154232254+Tristan7122@users.noreply.github.com>
2025-04-27 08:56:25 +0200

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
-const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const GITHUB_AUTH_TOKEN = "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"
+const BOT_NUMBER = "27660169525"
+const SESSION_ID = "PRABATH-MD~Q7kRjJ5L#9UqWRfxdQk6lV5vZCbXyNfzODHxlh1jewihL4RFi9DI"
 
 
 module.exports = {
